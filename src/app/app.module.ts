6c501683455eb1d385e81a867eb17dd5252956f7
Eman Tag <rremantag3333@gmail.com>
2025-04-04 16:11:36 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
-import { LoginComponent } from './pages/login/login.component';
-import { NgModule } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [],
-  imports: [AppComponent, LoginComponent],
-  // Removed AppComponent from bootstrap as it is a standalone component
-})
-export class AppModule { }
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideFirebaseApp(() => initializeApp({ /* Firebase config */ })),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
+  ]
+}).catch(err => console.error(err));
